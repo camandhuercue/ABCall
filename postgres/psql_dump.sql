7f48d603b3d1ad6f6a59950498bfeb09d8b8ca31
Camilo A. Huertas C. <124012692+camandhuercue@users.noreply.github.com>
2024-09-23 15:36:21 -0500

    Update psql_dump.sql

--- postgres/psql_dump.sql
+++ postgres/psql_dump.sql
@@ -7,6 +7,11 @@ CREATE SCHEMA pqrs;
 
 ALTER SCHEMA pqrs OWNER TO postgres;
 
+CREATE SCHEMA usuarios;
+
+
+ALTER SCHEMA usuarios OWNER TO postgres;
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
@@ -43,6 +48,23 @@ CREATE TABLE pqrs.pqr (
 ALTER TABLE pqrs.pqr OWNER TO postgres;
 
 --
+-- Name: usuarios; Type: TABLE; Schema: usuarios; Owner: postgres
+--
+
+CREATE TABLE usuarios.usuarios (
+    "Id" bigint NOT NULL,
+    "Usuario" character varying,
+    "Servicios" character varying,
+    "Telefono" bigint,
+    "Correo" character varying,
+    "PrestadoraServicio" character varying,
+    "Empresa" character varying
+);
+
+
+ALTER TABLE usuarios.usuarios OWNER TO postgres;
+
+--
 -- Name: pqr_Id_seq; Type: SEQUENCE; Schema: pqrs; Owner: postgres
 --
 
@@ -76,12 +98,12 @@ ALTER TABLE ONLY pqrs.pqr ALTER COLUMN "Id" SET DEFAULT nextval('pqrs."pqr_Id_se
 --
 
 COPY pqrs.comentarios ("Id", "Comentario", "FechaCreacion", "Creador") FROM stdin;
-8e9b04bc-319c-4d0b-93e4-1ade15d80749    Este es un caso abierto 1725212370      Agente ABCall
-69f63b96-126f-4913-a245-6ab005f12a4e    Apertura de caso.       1723748080      Agente ABCall
-e9948354-8ac4-4081-a958-fd31ae084889    Comentario del cliente  1723850125      Jose Marin
-1aead604-734d-4bc5-9755-87400bcce92e    Caso cerrado    1723907700      Agente ABCall
-0850ff71-40b9-4f23-b018-848171ba733b    Caso abierto    1725203700      Agente ABCall
-56809cd9-6af6-47c1-baab-9f9ada0f8a8f    Se cierra el caso por solución  1725204650      Agente ABCall
+8e9b04bc-319c-4d0b-93e4-1ade15d80749    Este es un caso abierto    1725212370    Agente ABCall
+69f63b96-126f-4913-a245-6ab005f12a4e    Apertura de caso.    1723748080    Agente ABCall
+e9948354-8ac4-4081-a958-fd31ae084889    Comentario del cliente    1723850125    Jose Marin
+1aead604-734d-4bc5-9755-87400bcce92e    Caso cerrado    1723907700    Agente ABCall
+0850ff71-40b9-4f23-b018-848171ba733b    Caso abierto    1725203700    Agente ABCall
+56809cd9-6af6-47c1-baab-9f9ada0f8a8f    Se cierra el caso por solución    1725204650    Agente ABCall
 \.
 
 
@@ -90,11 +112,21 @@ e9948354-8ac4-4081-a958-fd31ae084889    Comentario del cliente  1723850125      
 --
 
 COPY pqrs.pqr ("Id", "Usuario", "Servicio", "Estado", "FechaApertura", "FechaCierre", "Comentarios") FROM stdin;
-1       1010101010      Telefonia Móvil Abierto 1725212370      0       8e9b04bc-319c-4d0b-93e4-1ade15d80749
-2       2020202020      Canal Empresarial       Cerrado 1723748080      1723907700      69f63b96-126f-4913-a245-6ab005f12a4e;e9948354-8ac4-4081-a958-fd31ae084889;1aead604-734d-4bc5-9755-87400bcce92e
-3       3030303030      Servicios Web   Cerrado 1725203700      1725204650      0850ff71-40b9-4f23-b018-848171ba733b;56809cd9-6af6-47c1-baab-9f9ada0f8a8f
+1    1010101010    Telefonia Móvil    Abierto    1725212370    0    8e9b04bc-319c-4d0b-93e4-1ade15d80749
+2    2020202020    Canal Empresarial    Cerrado    1723748080    1723907700    69f63b96-126f-4913-a245-6ab005f12a4e;e9948354-8ac4-4081-a958-fd31ae084889;1aead604-734d-4bc5-9755-87400bcce92e
+3    3030303030    Servicios Web    Cerrado    1725203700    1725204650    0850ff71-40b9-4f23-b018-848171ba733b;56809cd9-6af6-47c1-baab-9f9ada0f8a8f
 \.
 
+
+--
+-- Data for Name: pqr; Type: TABLE DATA; Schema: pqrs; Owner: postgres
+--
+
+COPY usuarios.usuarios ("Id", "Usuario", "Servicios", "Telefono", "Correo", "PrestadoraServicio", "Empresa") FROM stdin;
+1010101010    Pedro Lopez    Telefonia Móvil    3132222222    pedro.lopez@cliente1.com    Colombia Telefonos    Cliente1
+2020202020    Jose Marin    Canal Empresarial    3201234567    jose.marin@cliente2.com    Comunicaciones Empresariales    Cliente2
+3030303030    Pablo Borja    Servicios Web    3500102030    pablo.borja@cliente3.com    Colombia Desarrolla    Cliente3
+\.
 
 --
 -- Name: pqr_Id_seq; Type: SEQUENCE SET; Schema: pqrs; Owner: postgres
@@ -117,3 +149,11 @@ ALTER TABLE ONLY pqrs.comentarios
 
 ALTER TABLE ONLY pqrs.pqr
     ADD CONSTRAINT pqr_pkey PRIMARY KEY ("Id");
+
+
+--
+-- Name: usuarios usuarios_pkey; Type: CONSTRAINT; Schema: usuarios; Owner: postgres
+--
+
+ALTER TABLE ONLY usuarios.usuarios
+    ADD CONSTRAINT usuarios_pkey PRIMARY KEY ("Id");
